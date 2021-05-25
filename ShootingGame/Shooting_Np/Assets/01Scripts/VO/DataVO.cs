0f0a7773ac8444f490cd8d53af48ad45562ee621
hwanjuson <hwanjuson@gmail.com>
2021-05-25 22:00:50 +0900

    다 없앰

--- ShootingGame/Shooting_Np/Assets/01Scripts/VO/DataVO.cs
+++
@@ -1,5 +0,0 @@
-public class DataVO
-{
-    public string type;
-    public string payload;
-}
--- ShootingGame/Shooting_Np/Assets/01Scripts/VO/RefreshVO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using System;
-
-[Serializable]
-class RefreshVO
-{
-    
-    public List<TransformVO> dataList;
-}
