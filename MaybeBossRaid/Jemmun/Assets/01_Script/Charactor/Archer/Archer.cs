954a85102275a328205b86ff00953006ea3a4a63
박상빈 <jixg20@gmail.com>
2021-07-21 01:55:25 +0900

    아처 코드

--- MaybeBossRaid/Jemmun/Assets/01_Script/Charactor/Archer/Archer.cs
+++ MaybeBossRaid/Jemmun/Assets/01_Script/Charactor/Archer/Archer.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class Archer : CharactorBase
 {
-    // private void SetVariable(){
-    //     hp = 80;
-    //     mp = 120;
-    //     pos = 0;
-    //     atk = 1.25f;
-    //     def = 1.2f;
-    // }
-    // //public override void OnDamage(int damage)
-    // //{
-    // //    hp -= damage;
-    // //    Debug.Log($"Hit, hp: {hp}, damage: {damage}");
-    // //}
+   
 }
