1002e84ccc09e9590b1c909e5537baab25670e8b
SnghyunCheon <a44306889@gmail.com>
2021-07-21 14:17:58 +0900

    전사 스킬 '용기의 함성' 이펙트 추가

--- MaybeBossRaid/Jemmun/Assets/01_Script/Skills/WarriorSkills.cs
+++ MaybeBossRaid/Jemmun/Assets/01_Script/Skills/WarriorSkills.cs
@@ -8,6 +8,9 @@ public class WarriorSkills : Skills
     private CharactorBase teammateBase = null;
     SkillData warriorAtk;
     SkillData warriorDef;
+    GameObject[] effectObject = new GameObject[2];
+    SpriteRenderer[] effectSpriteRenderer = new SpriteRenderer[2];
+    [SerializeField] Sprite effectSprite = null;
 
 
     sealed protected override void Awake()
@@ -33,6 +36,10 @@ public class WarriorSkills : Skills
         SkillManager.instance.SetSkillData(warriorAtk, SkillEnum.Wield);
         SkillManager.instance.SetSkillData(warriorDef, SkillEnum.BraveShout);
     }
+
+    /// <summary>
+    /// 디버그용 코드
+    /// </summary>
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -40,8 +47,10 @@ public class WarriorSkills : Skills
         if (teammateBase == null) return;
         if (TurnManager.instance.turn == defTurn + 2)
         {
+            StartCoroutine(ShieldEffect(teammateBase.gameObject, false));
             teammateBase.def = 1;
             this.charactor.def = 1;
+            defTurn = -1;
         }
     }
 
@@ -66,6 +75,7 @@ public class WarriorSkills : Skills
 
     protected sealed override void OnSkillBHit(CharactorBase targetBase)
     {
+        StartCoroutine(ShieldEffect(targetBase.gameObject, true));
         defTurn = TurnManager.instance.turn;
         teammateBase = targetBase;
         teammateBase.def = 0.8f;
@@ -74,5 +84,88 @@ public class WarriorSkills : Skills
         Debug.Log($"{targetBase.gameObject.name}: HitB!");
 
         // 화려한 이팩트 뭔가 일단 무언가 부와앙앍 하는것들
+    }
+
+    private IEnumerator ShieldEffect(GameObject target, bool doAppear)
+    {
+        float colorOP;
+        float size;
+        if (!CheckChild(this.gameObject, "Shield"))
+        {
+            effectObject[0] = Instantiate(new GameObject("Shield"), this.transform);
+        }
+        else
+            effectObject[0].SetActive(true);
+        if (!CheckChild(target.gameObject, "Shield"))
+        {
+            effectObject[1] = Instantiate(new GameObject("Shield"), target.transform);
+        }
+        else
+        {
+            effectObject[1] = FindChild(target.gameObject, "Shield");
+            effectObject[1].SetActive(true);
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            effectObject[i].name = "Shield";
+            if(effectObject[i].GetComponent<SpriteRenderer>() == null)
+                effectSpriteRenderer[i] = effectObject[i].AddComponent<SpriteRenderer>();
+            else
+                effectSpriteRenderer[i] = effectObject[i].GetComponent<SpriteRenderer>();
+            effectSpriteRenderer[i].sprite = effectSprite;
+        }
+
+        switch (doAppear)
+        {
+            case true:
+                colorOP = 0f;
+                size = 1f;
+                for (int i = 0; i < 50; ++i)
+                {
+                    for(int j = 0; j < 2; ++j)
+                    {
+                        effectSpriteRenderer[j].color = new Color(1, 1, 1, colorOP += 0.008f);
+                        effectObject[j].transform.localScale = new Vector2(size += 0.01f, size += 0.01f);
+                    }
+                    yield return new WaitForSeconds(0.01f);
+                }
+                break;
+
+            case false:
+                colorOP = 0.8f;
+                size = 3f;
+                for (int i = 0; i < 50; ++i)
+                {
+                    for (int j = 0; j < 2; ++j)
+                    {
+                        effectSpriteRenderer[j].color = new Color(1, 1, 1, colorOP -= 0.008f);
+                        effectObject[j].transform.localScale = new Vector2(size -= 0.01f, size -= 0.01f);
+                    }
+                    yield return new WaitForSeconds(0.01f);
+                }
+                for (int i = 0; i < 2; ++i)
+                    effectObject[i].SetActive(false);
+                break;
+        }
+    }
+
+    private bool CheckChild(GameObject target, string name)
+    {
+        for (int i = 0; i < target.transform.childCount; i++)
+        {
+            if (target.transform.GetChild(i).name == name)
+                return true;
+        }
+        return false;
+    }
+
+    private GameObject FindChild(GameObject target, string name)
+    {
+        for (int i = 0; i < target.transform.childCount; i++)
+        {
+            if (target.transform.GetChild(i).name == name)
+                return target.transform.GetChild(i).gameObject;
+        }
+        return null;
     }
 }
