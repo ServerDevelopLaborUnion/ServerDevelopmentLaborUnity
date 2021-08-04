53ffa4d67b3840ceace264f1b77fe4462c4bfbc6
Han <wooyeophan@gmail.com>
2021-08-04 23:02:18 +0900

    SOLID 원칙을 공부하면서 어찌어찌 클래스를 만들어내고 잇스빈다.

---
+++ FPS_Base/Client/JemmunFPS/Assets/001_Scripts/_Base/Charactor/CharactorData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public abstract class CharactorData
+{
+    public int speed
+    {
+        get => default;
+        set
+        {
+        }
+    }
+}
