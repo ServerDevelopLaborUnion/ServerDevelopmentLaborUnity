fa8185c6155a565e889b3db17a2b56d1e4fe40a1
HanUseArch <wooyeophan@gmail.com>
2021-09-15 00:09:18 +0900

    내일 할 거 연습한거 푸쉬 *아치 다시 깔꺼라서*  여러분 기장이 이렇게 준비 합니다.

---
+++ Class (사본)/SimpleChat/Assets/001_Script/ChatVO.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class ChatVO
+{
+    public string data;
+
+    public ChatVO(string data)
+    {
+        this.data = data;
+    }
+}
---
+++ Class (사본)/SimpleChat/Assets/001_Script/NickVO.cs
@@ -0,0 +1,12 @@
+using System;
+
+[Serializable]
+public class NickVO
+{
+    public string nickname;
+
+    public NickVO(string nickname)
+    {
+        this.nickname = nickname;
+    }
+}
