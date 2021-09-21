5e893ae44acb2a112d7abdc43cb6f574f2f9bcc5
HanUseArch <wooyeophan@gmail.com>
2021-09-21 23:31:04 +0900

    서버 폴더 안에서 파일 추가해서 안 들어간것이엇다. 후...

---
+++ SDLU_Shooting/Assets/001_Scripts/Socket/SocketClient.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+// 보내고 받는 기능이 있는 클라이언트
+
+public class SocketClient : MonoBehaviour
+{
+    static SocketClient inst = null;
+
+    private void Awake()
+    {
+        inst = this;
+
+        Connect("localhost", "32000"); // TODO : Debug code
+    }
+
+    private WebSocket ws; // connect 에서 인스턴스 할것
+
+
+
+
+    static public void Connect(string ip, string port) // 나중에는 json 에 있는 파일을 읽어서 ip, url 입력할 필요 없게 해봐도 좋을 듯 함
+    {
+        if(inst.ws != null && inst.ws.IsAlive)
+        {
+            Debug.Log("Already Connected to server. aborting");
+            return;
+        }
+
+        inst.ws = new WebSocket($"ws://{ip}:{port}");
+
+        inst.ws.Connect();
+
+        inst.ws.OnMessage += (socket, e) => {
+            inst.RecvData((WebSocket)socket, e);
+        };
+
+        Send(new DataVO("null", "wa sans")); // TODO : Debug code
+    }
+    private void RecvData(WebSocket sender, MessageEventArgs message)
+    {
+        // TODO : Toss data to bufferhandler
+        Debug.Log(message.Data);
+    }
+    
+#region Send Function
+
+    ///<summary>
+    ///Sends message to server.
+    ///</summary>
+    static public void Send(string msg)
+    {
+        inst.ws.Send(msg);
+    }
+
+    ///<summary>
+    ///Automaticly converts DataVO to json, and sends buffer to server.
+    ///</summary>
+    static public void Send(DataVO vo)
+    {
+        inst.ws.Send(JsonUtility.ToJson(vo));
+    }
+
+#endregion
+
+}
