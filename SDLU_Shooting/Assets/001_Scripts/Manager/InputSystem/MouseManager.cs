f8dff81ad45f19fa1e584d6e9e87c76d07c96b50
hun1001 <l01022899266@gmail.com>
2021-10-13 19:32:35 +0900

    hun1001
    중간 푸쉬

--- SDLU_Shooting/Assets/001_Scripts/Manager/InputSystem/MouseManager.cs
+++ SDLU_Shooting/Assets/001_Scripts/Manager/InputSystem/MouseManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
-    static private MouseManager inst = null;
+    static public MouseManager inst = null;
 
     private void Awake()
     {
