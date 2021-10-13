ba780f4ae9eb31ac785d397239c9d425400d1c01
hun1001 <l01022899266@gmail.com>
2021-10-13 20:27:20 +0900

    hun1001
    유니티 입력만

--- SDLU_Shooting/Assets/001_Scripts/Manager/Chatting/Chat.cs
+++ SDLU_Shooting/Assets/001_Scripts/Manager/Chatting/Chat.cs
@@ -9,21 +9,45 @@ public class Chat : MonoBehaviour
     private GameObject chatting = null;
 
     private InputField chatInput = null;
+    private Text fieldHolder = null;
+
+    private string holderText = "ÀüÃª";
 
     private bool chatActive = false;
+    private byte chatType = 0;
 
     void Start()
     {
-        chatting.SetActive(chatActive);
         chatInput = chatting.gameObject.transform.GetChild(1).GetComponent<InputField>();
+        fieldHolder = chatting.gameObject.transform.GetChild(1).gameObject.transform.GetChild(0).GetComponent<Text>();
+        fieldHolder.text = holderText;
+        chatting.SetActive(chatActive);
     }
 
     void Update()
     {
+        if (chatActive && Input.GetKeyDown(KeyCode.Tab))
+        {
+            ChangeChatType();
+            switch (chatType)
+            {
+                case 0:
+                    holderText = "ÀüÃÂ";
+                    break;
+                case 1:
+                    holderText = "±Ó¸»";
+                    break;
+                default:
+                    Debug.Log("Not making");
+                    holderText = "¿À·ù";
+                    break;
+            }
+            fieldHolder.text = holderText;
+        }
         if (Input.GetKeyDown(KeyCode.Return))
         {
             SetChatActive();
-        }
+        } 
     }
 
     private void SetChatActive()
@@ -42,6 +66,15 @@ public class Chat : MonoBehaviour
         else
         {
             chatInput.ActivateInputField();
+        }
+    }
+
+    private void ChangeChatType()
+    {
+        chatType++;
+        if (chatType > 1 || chatType < 0)
+        {
+            chatType = 0;
         }
     }
 
