f24d03daef1f48ed62ce0aff5727403bdfcd7f10
HanUseArch <wooyeophan@gmail.com>
2021-10-14 20:15:00 +0900

    Added Handler file

---
+++ SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Handler/ChatHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatHandler : MonoBehaviour
+{
+    void Start()
+    {
+        BufferHandler.Instance.AddHandler("msg", (msg) => {
+            Debug.Log(msg);
+        });
+    }
+}
