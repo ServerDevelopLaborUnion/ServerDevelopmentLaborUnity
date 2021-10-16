b96a8009637ae9fa4b909d2907e612179728de0f
HanUseArch <wooyeophan@gmail.com>
2021-10-16 23:43:16 +0900

    컴파일 에러 고쳐둠

--- SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Handler/ConnectionHandler.cs
+++ SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Handler/ConnectionHandler.cs
@@ -13,17 +13,17 @@ public class ConnectionHandler : MonoBehaviour
         BufferHandler.Instance.AddHandler("init", (data) => {
 
             int.TryParse(data,out int num);
-            GameManager.instance.playerBase.ID = num;
-            Debug.Log("플레이어 ID: " + GameManager.instance.playerBase.ID);
+            // GameManager.instance.playerBase.ID = num;
+            // Debug.Log("플레이어 ID: " + GameManager.instance.playerBase.ID);
         });
 
         BufferHandler.Instance.AddHandler("connect", (data) => {
-            InitVO vo = JsonUtility.FromJson<InitVO>(data);
-            if (vo.id == GameManager.instance.playerBase.ID) { Debug.Log("자신의 접속 데이터, 무시함."); return; } // ID 같다고 전부 다 걸러버리면 좀 문제가 생겨서
+            // InitVO vo = JsonUtility.FromJson<InitVO>(data);
+            // if (vo.id == GameManager.instance.playerBase.ID) { Debug.Log("자신의 접속 데이터, 무시함."); return; } // ID 같다고 전부 다 걸러버리면 좀 문제가 생겨서
             
-            CharactorBase charactor = Instantiate(connectionPlayerPrefab, Vector3.zero, Quaternion.identity).GetComponent<CharactorBase>();
-            charactor.ID = vo.id;
-            charactor.IsRemote = true;
+            // CharactorBase charactor = Instantiate(connectionPlayerPrefab, Vector3.zero, Quaternion.identity).GetComponent<CharactorBase>();
+            // charactor.ID = vo.id;
+            // charactor.IsRemote = true;
         });
     }
 }
