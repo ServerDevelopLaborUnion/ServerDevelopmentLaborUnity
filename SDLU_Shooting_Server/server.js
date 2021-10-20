aa6af46c4b6898d37bf181e4e52c6dd0b5825ad0
박상빈 <jixg20@gmail.com>
2021-10-20 19:33:51 +0900

    발상빈(발사처리)

--- SDLU_Shooting_Server/server.js
+++ SDLU_Shooting_Server/server.js
@@ -69,7 +69,9 @@ wsServer.on("connection", socket => {
                     case "damage":
                         broadcast(wsServer, socket, JSON.stringify(new DataVO("damage", payload)));
                         break;
-
+                    case "shoot":
+                        broadcast(wsServer, socket, JSON.stringify(new DataVO("shoot", payload)));
+                        break;
                     default:
                         socket.send(JSON.stringify(new DataVO("errmsg", "그런 타입이 없습니다.")));
                 }
