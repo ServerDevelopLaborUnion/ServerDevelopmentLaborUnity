c348e612aa221ce0823b4e8fcd423f632f0c2712
HanUseArch <wooyeophan@gmail.com>
2021-10-20 20:27:40 +0900

    접속 계속 만드는 중

--- SDLU_Shooting_Server/Handlers/UserConnectionHandler.js
+++ SDLU_Shooting_Server/Handlers/UserConnectionHandler.js
@@ -7,12 +7,16 @@ const { DataVO } = require("../VO/DataVO.js");
 
 function userConnectedHandler(wsServer, socket)
 {
-    broadcast(wsServer, socket, JSON.stringify({
-                                id: socket.sessionId,
-                                pos: spawnPositions[Math.random() * (spawnPositions.length - 1)],
-                                hp: DefaultValue.hp
-                                }
-    )); // 문자열로 바꿔줌
+
+    broadcast(wsServer, socket, JSON.stringify(new DataVO("connect",
+                                JSON.stringify({
+                                    id: socket.sessionId, // 여기까지만 JSON 으로 만들어짐
+                                    pos: spawnPositions[Math.random() * (spawnPositions.length - 1)],
+                                    hp: DefaultValue.hp
+                                })
+            )
+        )
+    ); // 문자열로 바꿔줌
 
 }
 
