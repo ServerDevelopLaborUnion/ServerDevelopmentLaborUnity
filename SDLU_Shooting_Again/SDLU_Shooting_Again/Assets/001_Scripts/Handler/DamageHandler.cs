fb84636ca4515b69a0b3ca6c0d6f6bdd77d6cfb7
14013com <leepaul050210@gmail.com>
2021-10-28 20:17:06 +0900

    자기 데미지 또받는거 막기

--- SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Handler/DamageHandler.cs
+++ SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Handler/DamageHandler.cs
@@ -9,6 +9,11 @@ public class DamageHandler : MonoBehaviour
         BufferHandler.Instance.AddHandler("damage", (data) =>
         {
             DamageVO vo = JsonUtility.FromJson<DamageVO>(data);
+            
+            if (GameManager.Instance.Player.ID == vo.id)
+            {
+                return;
+            }
 
             UserManager.Instance.Get(vo.id).Damaged(vo.damage);
             //TODO : damaged animation
