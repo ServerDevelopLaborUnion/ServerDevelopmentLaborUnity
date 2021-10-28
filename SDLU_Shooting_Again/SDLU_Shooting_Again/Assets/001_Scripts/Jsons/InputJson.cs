7cf1af19ed844464977ff3e4a916c6140b97671a
HanUseArch <wooyeophan@gmail.com>
2021-10-28 19:48:43 +0900

    폴더 만든건 커밋해야할거같았음

---
+++ SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Jsons/InputJson.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public enum MouseInput
+{
+    Left = 0,
+    Right,
+    Wheel
+}
+
+
+
+[Serializable]
+public class InputJson
+{
+    // 키보드 입력
+    public KeyCode foward    = KeyCode.W;
+    public KeyCode backward  = KeyCode.S;
+    public KeyCode left      = KeyCode.A;
+    public KeyCode right     = KeyCode.D;
+    public KeyCode rollLeft  = KeyCode.Q;
+    public KeyCode rollRight = KeyCode.E;
+    public KeyCode up        = KeyCode.Space;
+    public KeyCode down      = KeyCode.LeftShift;
+
+    // 마우스 입력
+    public MouseButton fire = MouseButton.Left;
+    public MouseButton aim  = MouseButton.Right;
+}
