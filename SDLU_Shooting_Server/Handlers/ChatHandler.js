62a72896afce19e913fb483c10ca12c9d1af4a2c
KBluePurple <42506968+BlWon@users.noreply.github.com>
2021-11-03 19:54:36 +0900

    코드정리

--- SDLU_Shooting_Server/Handlers/ChatHandler.js
+++ SDLU_Shooting_Server/Handlers/ChatHandler.js
@@ -1,15 +1,13 @@
 const { DataVO } = require("../VO/DataVO.js");
 const { broadcast } = require("../Utils/Broadcast.js");
 
-class ChatHandler
-{
-    Chat(socket, payload)
-    {
+class ChatHandler {
+    Chat(socket, payload) {
+        /** @type {string} */
         var message = payload;
-        message = socket.user.nickname + ": " + message;
 
-        if(message)
-        {
+        if (message.trim() != "" && message != null) {
+            message = socket.user.nickname + ": " + message;
             var dataVO = new DataVO("chat", message);
             broadcast(JSON.stringify(dataVO));
         }
