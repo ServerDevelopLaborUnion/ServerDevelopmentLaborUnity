007b567fb3fee4ff5fc334ddd2b5ff1f5dc5ad9f
박상빈 <jixg20@gmail.com>
2021-11-03 20:32:37 +0900

    발상빈

--- SDLU_Shooting/Assets/001_Scripts/Manager/ShootManager.cs
+++ SDLU_Shooting/Assets/001_Scripts/Manager/ShootManager.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 
 public class ShootManager : MonoBehaviour
 {
+    [SerializeField] private Transform firePos;
     void Update()
     {
-        // if(Input.GetKeyDown(KeyCode.Space)){
-        //     SocketClient.Instance.Send(new DataVO("shoot", JsonUtility.ToJson(new ShootVO(GameManager.instance.playerBase.ID , GameManager.instance.player.eulerAngles ,GameManager.instance.player.position))));
-        // }
         if(Input.GetKeyDown(KeyCode.Space)){
-            Debug.Log(GameManager.instance.playerBase.ID);
-            SocketClient.Instance.Send(new DataVO("dead" , JsonUtility.ToJson(new DeadVO(GameManager.instance.playerBase.ID))));
-            Debug.Log("뎀지 입음");
+            SocketClient.Instance.Send(new DataVO("shoot", JsonUtility.ToJson(new ShootVO(GameManager.instance.playerBase.ID , GameManager.instance.player.eulerAngles ,firePos.position))));
         }
+        // if(Input.GetKeyDown(KeyCode.Space)){
+        //     Debug.Log(GameManager.instance.playerBase.ID);
+        //     SocketClient.Instance.Send(new DataVO("dead" , JsonUtility.ToJson(new DeadVO(GameManager.instance.playerBase.ID))));
+        //     Debug.Log("뎀지 입음");
+        // }
     }
 }
