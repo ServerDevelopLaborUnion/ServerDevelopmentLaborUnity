2a52d50818417ccbffebaefb3c19cabac4a4ac92
HanUseArch <wooyeophan@gmail.com>
2021-11-04 11:33:45 +0900

    이동은 이제 자연스럽게 되는데 아직 먼저 들어간 사람에게는 이동이 안되는 버그 있음

--- SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Players/RemotePlayer/RemotePlayer.cs
+++ SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Players/RemotePlayer/RemotePlayer.cs
@@ -7,24 +7,29 @@ public class RemotePlayer : CharactorBase
     [SerializeField] private float lerpAmount = 0.7f;
 
     Vector3 targetPos = new Vector3();
-    Vector3 tartetRot = new Vector3();
+    Vector3 targetRot = new Vector3();
 
     private void Awake() {
         targetPos = transform.position;
-        tartetRot = transform.eulerAngles;
+        targetRot = transform.eulerAngles;
     }
 
 
     public void SetTargetTransform(Vector3 pos, Vector3 rot)
     {
         targetPos = pos;
-        tartetRot = rot;
+        targetRot = rot;
     }
 
 
     private void Update()
     {
-        transform.position    = Vector3.Lerp(transform.position,    targetPos, lerpAmount);
-        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetPos, lerpAmount);
+        if(Vector3.Distance(targetPos, transform.position) > 0.01f)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPos, lerpAmount);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(targetRot), lerpAmount);
+
     }
 }
