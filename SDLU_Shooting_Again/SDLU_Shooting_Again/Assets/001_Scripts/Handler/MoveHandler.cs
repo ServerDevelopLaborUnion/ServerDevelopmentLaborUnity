5e97913889941a0d0d0494738fc4ba3f1ad28a0d
HanUseArch <wooyeophan@gmail.com>
2021-11-04 11:19:33 +0900

    이동 부드럽게 수정

--- SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Handler/MoveHandler.cs
+++ SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Handler/MoveHandler.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class MoveHandler : MonoBehaviour
 {
-    [SerializeField] float lerpAmount = 0.7f;
-
     private void Start()
     {
         BufferHandler.Instance.AddHandler("move",(msg) =>
@@ -16,18 +14,13 @@ public class MoveHandler : MonoBehaviour
                 return;
             }
 
-            Transform targetTrm = UserManager.Instance.Get(vo.id).gameObject.transform;
-            if(targetTrm == null) return;
+            RemotePlayer target = UserManager.Instance.Get(vo.id).gameObject.transform.GetComponent<RemotePlayer>();
+            if(target == null) return;
 
             Vector3 newPos  = JsonUtility.FromJson<Vector3>(vo.pos);
-            Vector3 lastPos = targetTrm.position;
-
             Vector3 newRot  = JsonUtility.FromJson<Vector3>(vo.rot);
-            Vector3 lastRot = targetTrm.eulerAngles;
 
-            targetTrm.position    = Vector3.Lerp(lastPos, newPos, lerpAmount);
-            targetTrm.eulerAngles = Vector3.Lerp(lastRot, newRot, lerpAmount);
-
+            target.SetTargetTransform(newPos, newRot);
 
         });
     }
--- SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Players/RemotePlayer/RemotePlayer.cs
+++ SDLU_Shooting_Again/SDLU_Shooting_Again/Assets/001_Scripts/Players/RemotePlayer/RemotePlayer.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class RemotePlayer : CharactorBase
 {
-    
+    [SerializeField] private float lerpAmount = 0.7f;
+
+    Vector3 targetPos = new Vector3();
+    Vector3 tartetRot = new Vector3();
+
+
+    public void SetTargetTransform(Vector3 pos, Vector3 rot)
+    {
+        targetPos = pos;
+        tartetRot = rot;
+    }
+
+
+    private void Update()
+    {
+        transform.position    = Vector3.Lerp(transform.position,    targetPos, lerpAmount);
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetPos, lerpAmount);
+    }
 }
