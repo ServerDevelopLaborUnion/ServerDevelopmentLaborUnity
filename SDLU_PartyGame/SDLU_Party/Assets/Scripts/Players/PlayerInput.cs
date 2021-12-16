9161676aa28549a7107c336511095b9f299ce31f
HanUseArch <wooyeophan@gmail.com>
2021-12-16 20:24:14 +0900

    일단 입력은 만들어짐

--- SDLU_PartyGame/SDLU_Party/Assets/Scripts/Players/PlayerInput.cs
+++ SDLU_PartyGame/SDLU_Party/Assets/Scripts/Players/PlayerInput.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Action
+
+    /// <summary>
+    /// Called when left clicked
+    /// </summary>
+    public event Action OnMouseAttackButtonDown;
+    /// <summary>
+    /// Called when right clicked
+    /// </summary>
+    public event Action OnMouseMoveButtonDown;
+    /// <summary>
+    /// Called when wheel clicked
+    /// </summary>
+    public event Action OnMouseMiddleDown;
+
+    #endregion
+
+    InputMapping key = new InputMapping();
+
     void Start()
     {
-        
+        OnMouseAttackButtonDown += () => { };
+        OnMouseMoveButtonDown   += () => { };
+        OnMouseMiddleDown       += () => { };
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown((int)key.move))
+        {
+            OnMouseMoveButtonDown();
+        }
+
+        if(Input.GetMouseButtonDown((int)key.attack))
+        {
+            OnMouseAttackButtonDown();
+        }
+
+
     }
 }
