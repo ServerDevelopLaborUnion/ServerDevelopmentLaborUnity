b1e9a8e5d41e489dc6b1d5c922bd9c9c1241ef40
HanUseArch <wooyeophan@gmail.com>
2021-12-24 00:56:05 +0900

    예외 방지를 넣음

--- SDLU_PartyGameServer/util/database.js
+++ SDLU_PartyGameServer/util/database.js
@@ -4,9 +4,20 @@ const url = "http://127.0.0.1/SDLU_PartyGameDatabaseServer/";
 
 function sendPost(type, params) {
 
-    request.post(url, { form: { type: type } }, (err, res, body) => {
-        console.log(JSON.parse(body));
+    request.post(url, {
+        form: {
+            type: type,
+            data: params
+        }
+    }, (err, res, body) => {
+        try {
+            console.log(JSON.parse(body));
+        } catch (e) {
+            console.log("err\r\n" + body);
+        }
     });
 }
 
-sendPost("login");
+// 디버그
+// sendPost("register", JSON.stringify({ id: "jan", pw: "1234" }));
+sendPost("login", JSON.stringify({ id: "jan", pw: "1234" }));
